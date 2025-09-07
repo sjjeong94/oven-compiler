f44c2dd0a684dfda9a252948ea95389cacab20af
sjjeong94 <sjjeong94@gmail.com>
2025-09-07 11:58:04 +0000

    decompose sigmoid

--- lib/Dialect/Oven/Transform/OptimizeOven.cpp
+++ lib/Dialect/Oven/Transform/OptimizeOven.cpp
@@ -22,14 +22,31 @@ struct DecomposeExpPattern : public OpRewritePattern<math::ExpOp> {
 
   LogicalResult matchAndRewrite(math::ExpOp op,
                                 PatternRewriter &rewriter) const override {
-    arith::ConstantOp constant = rewriter.create<arith::ConstantOp>(
+    // exp(x) = 2^(x * log2(e)) where log2(e) ~= 1.442695
+    Value constant = rewriter.create<arith::ConstantOp>(
         op.getLoc(), rewriter.getF32FloatAttr(1.442695));
-    arith::MulFOp mul = rewriter.create<arith::MulFOp>(
-        op.getLoc(), op.getType(), op.getOperand(), constant.getResult());
-
-    math::Exp2Op exp2 = rewriter.create<math::Exp2Op>(op.getLoc(), op.getType(),
-                                               mul.getResult());
+    Value mul = rewriter.create<arith::MulFOp>(op.getLoc(), op.getType(),
+                                               op.getOperand(), constant);
+    Value exp2 = rewriter.create<math::Exp2Op>(op.getLoc(), op.getType(), mul);
     rewriter.replaceOp(op, exp2);
+    return success();
+  }
+};
+
+struct DecomposeSigmoidPattern : public OpRewritePattern<oven::SigmoidOp> {
+  DecomposeSigmoidPattern(mlir::MLIRContext *context)
+      : OpRewritePattern<oven::SigmoidOp>(context, /*benefit=*/1) {}
+
+  LogicalResult matchAndRewrite(oven::SigmoidOp op,
+                                PatternRewriter &rewriter) const override {
+    // sigmoid(x) = 1 / (1 + exp(-x))
+    Value negX = rewriter.create<arith::NegFOp>(op.getLoc(), op.getOperand());
+    Value expNegX = rewriter.create<math::ExpOp>(op.getLoc(), negX);
+    Value one = rewriter.create<arith::ConstantOp>(
+        op.getLoc(), rewriter.getF32FloatAttr(1.0));
+    Value denom = rewriter.create<arith::AddFOp>(op.getLoc(), one, expNegX);
+    Value result = rewriter.create<arith::DivFOp>(op.getLoc(), one, denom);
+    rewriter.replaceOp(op, result);
     return success();
   }
 };
@@ -40,6 +57,7 @@ struct OptimizeOven : impl::OptimizeOvenBase<OptimizeOven> {
   void runOnOperation() {
     mlir::RewritePatternSet patterns(&getContext());
     patterns.add<DecomposeExpPattern>(&getContext());
+    patterns.add<DecomposeSigmoidPattern>(&getContext());
     (void)applyPatternsGreedily(getOperation(), std::move(patterns));
   }
 };
--- tests/oven.mlir
+++ tests/oven.mlir
@@ -1,9 +1,4 @@
 
-func.func @test_llvm(%a: f32, %b: f32) -> (f32, f32) {
-  %0 = math.exp %a : f32
-  return %0, %b : f32, f32
-}
-
 func.func @test_load_store(%a: !llvm.ptr, %b: !llvm.ptr) {
   %0 = arith.constant 1024 : i32
   %1 = nvvm.read.ptx.sreg.ctaid.x : i32
@@ -13,4 +8,14 @@ func.func @test_load_store(%a: !llvm.ptr, %b: !llvm.ptr) {
   %9 = oven.load %a, %4 : (!llvm.ptr, i32) -> f32
   oven.store %9, %b, %4 : (f32, !llvm.ptr, i32)
   return
+}
+
+func.func @test_exp(%a: f32) -> f32 {
+  %0 = math.exp %a : f32
+  return %0 : f32
+}
+
+func.func @test_sigmoid(%a: f32) -> f32 {
+  %0 = oven.sigmoid %a : f32 -> f32
+  return %0 : f32
 }
